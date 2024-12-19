dc831d531e5e62b33b81feafa8ee129d7adb4102
ranzxii <164470661+ranzxi2345@users.noreply.github.com>
2024-12-19 19:19:05 +0800

    index.php

---
+++ ANIME/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RANZXI Anime</title>
+    <link rel="stylesheet" href="styles.css">
+  
+</head>
+
+<body>
+    <main>
+        <!-- Welcome Section -->
+        <div id="welcome-section">
+            <img src="./images/l1.png" alt="Logo" id="logo-img">
+
+            <div class="welcome-text">
+                <h1>Unlimited anime, movies, and more.</h1>
+                <h2>Watch your favorite series anytime.</h2>
+                 
+                <section>
+                    <div id="about-ranzxi">
+                        <h2>About RanZxi Anime</h2>
+                        <p>Welcome to RanZxi Anime, your ultimate online destination for a vast collection of anime and movies. At RanZxi, we bring you a rich library of diverse genres, including action, adventure, romance, fantasy, and more. Our platform is dedicated to providing you with the best in anime entertainment, from classic favorites to the latest releases.</p>
+                        <p>Join our community and experience seamless viewing with no interruptions. Watch anytime, anywhere, and never miss a moment of your favorite shows!</p>
+                    </div>
+                </section>
+
+                <!-- Get Started Button -->
+        <button id="get-started-button" onclick="location.href='../homepage.php'">Get Started ></button>
+
+            </div>
+        </div>
+
+        <footer>
+            <div class="footer-content">
+                <p>&copy; 2024 RanZxi Anime. All rights reserved.</p>
+                <p><a href="#" class="footer-link">Privacy Policy</a> | <a href="#" class="footer-link">Terms of Service</a></p>
+            </div>
+        </footer>
+    </main>
+
+
+    <script src="index.js"></script>
+</body>
+</html>
