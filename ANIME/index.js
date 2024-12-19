e4ada1e701ab3837c6e8c9af384df614799ecca5
ranzxii <164470661+ranzxi2345@users.noreply.github.com>
2024-12-19 19:19:36 +0800

    index.js

---
+++ ANIME/index.js
@@ -0,0 +1,9 @@
+// Get reference to the Get Started button
+const getStartedButton = document.getElementById('get-started-button');
+
+// Add a click event listener to the button
+getStartedButton.addEventListener('click', function() {
+    // Redirect to login page
+    window.location.href = 'homepage/homepage.php';
+});
+
